cd4d399bd50a728f55cb1e1b9a01157a516d0f9c
danielgarza <danielgarza22@gmail.com>
2022-02-14 22:00:17 -0500

    Worked on my SQL challenge analysis

--- schema.sql
+++ schema.sql
@@ -1,5 +1,3 @@
--- SQL Challenge
-
 CREATE TABLE departments(
     dept_no         VARCHAR(4)          NOT NULL,
     dept_name       VARCHAR(40)         NOT NULL,
@@ -46,50 +44,3 @@ CREATE TABLE salaries(
     PRIMARY KEY (emp_no, salary),
     FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
-
-DROP TABLE dept_manager;
-
-SELECT * FROM departments;
-SELECT * FROM titles;
-SELECT * FROM employees;
-SELECT * FROM dept_emp;
-SELECT * FROM dept_manager;
-SELECT * FROM salaries;
-
--- Data Analysis
--- 1. List the following details of each employee: employee number, last name, first name, sex, and salary.
-SELECT e.emp_no, e.last_name, e.first_name, s.salary
-FROM employees as e
-JOIN salaries as s
-ON (e.emp_no = s.emp_no);
-
--- 2. List first name, last name, and hire date for employees who were hired in 1986.
-SELECT first_name, last_name, hire_date
-FROM employees
-WHERE hire_date >= '1/1/1986' AND hire_date < '1/1/1987';
-
--- 3. List the manager of each department with the following information: department number,
--- department name, the manager's employee number, last name, first name.
-SELECT dm.dept_no, dm.dept_name, 
-FROM dept_manager as dm
-
-
--- 4. List the department of each employee with the following information: employee number,
--- last name, first name, and department name.
-
-
--- 5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
-
-
--- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
-
-
--- 7. List all employees in the Sales and Development departments, including their employee number,
--- last name, first name, and department name.
-
-
--- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
-
-
-
-
